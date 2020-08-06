1f98bf1776bfbd290ef45204b10d84d8a72e3f1e
chenzihao <654032921@qq.com>
2020-08-06 22:19:17 +0800

    fix:同步开启编写demo

---
+++ src/routes/IndexPage.js
@@ -0,0 +1,18 @@
+
+import React, { Component } from 'react'
+import styles from './IndexPage.css';
+
+class IndexPage extends Component {
+  render() {
+    return (
+      <div className={styles.container}>
+
+        <h2 className={styles.title}>欢迎来到echarts小课堂</h2>
+
+      </div>
+    )
+  }
+}
+
+export default IndexPage
+
